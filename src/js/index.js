c0d93e0d7f567da7bcc717129c7a69a1ee07048b
Diego Alejandro Aguilera Martinez <diego.aguilera@unillanos.edu.co>
2019-01-23 19:30:05 -0500

    primer concepto

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const header = document.getElementById('header');
+const logo = document.getElementById('logo');
+const menu = document.getElementById('menu');
+const hero = document.getElementById('hero');
+console.log(menu);
+
+document.addEventListener('scroll', e => {
+  // if (document.documentElement.scrollTop >= hero.clientHeight) {
+  //   console.log('Entró')
+  //   // logo.classList.add('color')
+  //   header.classList.remove('background-color-map')
+
+  //   // menu.classList.add('color')
+  // } else {
+  //   // logo.classList.remove('color')
+  //   // menu.classList.remove('color')
+  //   header.classList.add('background-color-map')
+  // }
+})
