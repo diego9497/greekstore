c13178ae7316d314a83933e608e54221ff8abf7a
Diego Alejandro Aguilera Martinez <diego.aguilera@unillanos.edu.co>
2019-01-23 20:58:48 -0500

    arreglo banner lateral fixed

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,17 @@ const hero = document.getElementById('hero');
 console.log(menu);
 
 document.addEventListener('scroll', e => {
-  // if (document.documentElement.scrollTop >= hero.clientHeight) {
-  //   console.log('Entró')
-  //   // logo.classList.add('color')
-  //   header.classList.remove('background-color-map')
+  if (document.documentElement.scrollTop >= hero.clientHeight) {
+    //   console.log('Entró')
+    //   // logo.classList.add('color')
+    //   header.classList.remove('background-color-map')
+    menu.classList.add('fixed')
 
-  //   // menu.classList.add('color')
-  // } else {
-  //   // logo.classList.remove('color')
-  //   // menu.classList.remove('color')
-  //   header.classList.add('background-color-map')
-  // }
+    //   // menu.classList.add('color')
+  } else {
+    //   // logo.classList.remove('color')
+    //   // menu.classList.remove('color')
+    //   header.classList.add('background-color-map')
+    menu.classList.remove('fixed')
+  }
 })
