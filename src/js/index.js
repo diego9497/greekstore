5c086731ba5d478c958ad4f397343e93b40f130d
Diego Alejandro Aguilera Martinez <diego.aguilera@unillanos.edu.co>
2019-02-10 15:06:06 -0500

    arreglo imagenes recomendacions, agregar feedback a botones

--- src/js/index.js
+++ src/js/index.js
@@ -118,6 +118,12 @@ botonesMenu.forEach(element => {
 
 const menuButton = document.getElementById('menu-button');
 const menuLateral = document.getElementById('menu-lateral');
+const line1 = document.getElementById('line1')
+const line2 = document.getElementById('line2')
+const line3 = document.getElementById('line3')
 menuButton.addEventListener('click', () => {
+  line1.classList.toggle('menu-button-line1')
+  line2.classList.toggle('menu-button-line2')
+  line3.classList.toggle('menu-button-line3')
   menuLateral.classList.toggle('show-menu-lateral')
 })
