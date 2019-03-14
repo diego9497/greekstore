199b67699fce2e800478cda30f05fba784833010
Diego Alejandro Aguilera Martinez <diego.aguilera@unillanos.edu.co>
2019-03-14 16:51:46 -0500

    Todos los filtros responsive, y cambios diseño

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,39 @@
 const menuFilter = document.querySelectorAll('.menu-filter-element-container')
-menuFilter.forEach(element => {
-  element.addEventListener('click', () => {
-    console.log(element);
-    element.querySelector('.menu-filter-options').classList.toggle('hide-options')
-    element.querySelector('i').classList.toggle('rotate')
+const menuContainer = document.getElementById('menu-container')
+const filterButton = document.getElementById('filter-button')
 
+console.log(menuContainer.offsetHeight)
+menuFilter.forEach(element => {
+  element.addEventListener('click', (e) => {
+    console.log(menuContainer.offsetHeight)
+    if (e.target.className !== "price-range-field") {
+      element.querySelector('.menu-filter-options').classList.toggle('hide-options')
+      element.querySelector('i').classList.toggle('rotate')
+    }
   })
-});
+})
+let esconderMostrar = () => {
+  menuContainer.classList.toggle("hide");
+}
+function modoMovil(x) {
+  if (x.matches) {
+    menuContainer.classList.add("hide");
+    filterButton.addEventListener('click', esconderMostrar)
+    menuFilter.forEach(element => {
+      element.querySelector('.menu-filter-options').classList.add('hide-options')
+      element.querySelector('i').classList.toggle('rotate')
+    })
+  } else {
+    menuContainer.classList.remove("hide");
+    filterButton.removeEventListener('click', esconderMostrar);
+    menuFilter.forEach(element => {
+      element.querySelector('.menu-filter-options').classList.remove('hide-options')
+      element.querySelector('i').classList.toggle('rotate')
+    })
+  }
+}
+let media = window.matchMedia("(max-width: 760px)")
+modoMovil(media)
+media.addListener(modoMovil)
+
+
